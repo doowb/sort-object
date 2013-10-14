a3ebb3542ce8f6668623cf783abaf3c125c28ac5
Jon Schlinkert <dev@sellside.com>
2013-10-13 20:29:24 -0400

    renames tests to test, to adhere to mocha convention. changes dependency to lodash

--- sort-object.js
+++ sort-object.js
@@ -5,17 +5,16 @@
  * Licensed under the MIT license.
  */
 
-var _extend = require('lodash.assign');
-var _keys   = require('lodash.keys');
+var _ = require('lodash');
 
 /**
  * Custom sort function to allow sorting by descending order
- * @param  {Object} options optional parameter specifying which order to sort in.
- * @return {Function}       function used to pass into a sort function.
+ * @param  {Object}     options optional parameter specifying which order to sort in.
+ * @return {Function}   function used to pass into a sort function.
  */
 var sortBy = function (options) {
 
-  var opts = _extend({ order: 'asc' }, options);
+  var opts = _.extend({ order: 'asc' }, options);
 
   return function (objA, objB) {
     var result = 0;
@@ -31,12 +30,12 @@ var sortBy = function (options) {
 /**
  * Sorts the ksys on an object
  * @param  {Object} obj     Object that has keys to be sorted
- * @param  {Object} options optional parameter specifying orders for the function
+ * @param  {Object} options Optional parameter specifying orders for the function
  * @return {Object}         Object with keys sorted
  */
-var sort = module.exports = function (obj, options) {
+function sort(obj, options) {
 
-  var opts = _extend({
+  var opts = _.extend({
     order: 'asc'
   }, options);
 
@@ -44,7 +43,7 @@ var sort = module.exports = function (obj, options) {
       keys   = [],
       key;
 
-  keys = _keys(obj);
+  keys = _.keys(obj);
   keys.sort(sortBy(options));
 
   for (var index in keys) {
@@ -53,5 +52,6 @@ var sort = module.exports = function (obj, options) {
   }
 
   return sorted;
-
 };
+
+module.exports = sort;
